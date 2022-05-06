991677a58b77816de2dd608a46f5638daf7e7bd1
realguy33 <70895323+realguy33@users.noreply.github.com>
2022-05-06 22:20:44 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -49,8 +49,8 @@ figcrvpol.update_layout(
 #stream lit work
 
 st.title("Uniswap Daily Swap Volume in USD on different L2s")
-st.write("Uniswap is DEX(Decentrailized Exchange) on Ethereum mainnet and it is also available on various L2s such Arbitrum,Optimism and polygon. The protocol uses a set of smart contracts to create liquidity pools and swap assets. Reserves and prices are updated every time a trade is made by way of a constant product formula. All of these eliminate the need for central intermediaries.")
-st.write("The advantage of using L2s over the ethereum mainnet is that the gas fees on L2s are much much lower than ethereum and these L2s also claim to have the same security as that  of ethereum. This makes them attractive to the users.This means that the usage of various L2s should slowly be increasing. is this true? let's find out:")
+st.write("Uniswap is DEX(Decentrailized Exchange) on Ethereum mainnet and it is also available on various L2s(Layer 2s) such Arbitrum,Optimism and polygon. The protocol uses a set of smart contracts to create liquidity pools and swap assets. Reserves and prices are updated every time a trade is made by way of a constant product formula. All of these eliminate the need for central intermediaries.")
+st.write("Layer 2s are separate blockchains that extends Ethereum.The advantage of using L2s over the ethereum mainnet is that the gas fees on L2s are much much lower than ethereum and these L2s also claim to have the same security as that  of ethereum. This allows them to scale better than ethereum mainnet and makes them attractive to the users.This means that the usage of various L2s should slowly be increasing. is this true? let's find out:")
 st.subheader("DATA SOURCES:")
 st.write("The data for this dashboard has been taken from various subgraphs available on thegraph.com.They are linked at the end of the dashboard")
 
